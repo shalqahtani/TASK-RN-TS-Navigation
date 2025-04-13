365fb1c9cd974d7d605e85d000212e03a7c5ab9a
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-04-13 14:05:09 +0300

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,7 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="StayIndex" />
-      <Stack.Screen name="Staydetails" />
-      <Stack.Screen name="TripDetails" />
-    </Stack>
-  );
+  return <Stack />;
 }
 
 export const unstable_settings = {
